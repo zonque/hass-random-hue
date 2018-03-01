0a91db7d06742cb69c4b336d1a9cb3cc41854837
Daniel Mack <daniel@zonque.org>
2018-03-01 06:56:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# hass-random-hue
+Home Assistant scene component that takes a random hue, calculates its RGB value with full saturation and applies it to one or more lights.
+
+## The HSV circle
+
+Colors are taken randomly from the HSV circle.
+
+![HSV](images/hsv.png)
+
+## Installation
+
+* If it doesn't exist yet, create a sub-folder in your configuration directory called `custom_components/scene`
+* Download the [random_hue.py](random_hue.py) file and copy it to this sub-folder
+
+## Configuration
+
+Add the following snippet to your `configuration.yaml`, or any file that is included by it:
+
+```
+scene:
+  - platform: random_hue
+    name: Random Colors
+    lights:
+      - light.RGB1
+      - light.RGB2
+    same_color: false
+    transition: 1
+```
+
+### Parameters
+
+* `name` (optional): the name of this scene
+* `lights` (required): list of lights to apply random color values to
+* `same_color` (optional): when `true`, all lights will be set to the same color. Otherwise, individual random colors are chosen.
+* `transition` (optional): numeric value for the transition time
+
+Note that the color of the lights is only changed if the lights are already switched on.
+
+### Automation
+
+Example automation to change the color to new random values every 5 minutes:
+
+```
+automation:
+  - alias: Random color every 5 minutes
+    trigger:
+      platform: time
+      minutes: '/5'
+      seconds: '0'
+    action:
+      service: scene.turn_on
+      entity_id: scene.random_colors
+```
+
+## License
+
+GPLv2
+
---
+++ random_hue.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+import colorsys
+import random
+
+import voluptuous as vol
+
+from homeassistant.components.scene import Scene
+from homeassistant.const import CONF_PLATFORM, CONF_NAME
+import homeassistant.helpers.config_validation as cv
+import homeassistant.components.light as light
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_LIGHTS = 'lights'
+CONF_TRANSITION = 'transition'
+CONF_SAME_COLOR = 'same_color'
+
+PLATFORM_SCHEMA = vol.Schema({
+    vol.Required(CONF_PLATFORM): 'random_hue',
+    vol.Optional(CONF_NAME): cv.string,
+    vol.Required(CONF_LIGHTS): cv.entity_ids,
+    vol.Optional(CONF_TRANSITION): cv.positive_int,
+    vol.Optional(CONF_SAME_COLOR): cv.boolean,
+})
+
+# pylint: disable=unused-argument
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
+    name = config.get(CONF_NAME, "Random Color")
+    lights = config.get(CONF_LIGHTS)
+    transition = config.get(CONF_TRANSITION, 0)
+    same_color = config.get(CONF_SAME_COLOR, False)
+
+    scene = RandomHue(hass, name, lights, transition, same_color)
+    async_add_devices([scene])
+    return True
+
+class RandomHue(Scene):
+    def __init__(self, hass, name, lights, transition, same_color):
+        self.hass = hass
+        self._name = name
+        self._lights = lights
+        self._transition = transition
+        self._same_color = same_color
+
+    @property
+    def name(self):
+        return self._name
+
+    @asyncio.coroutine
+    def async_activate(self):
+        rgb = None
+
+        for entity in self._lights:
+            if not light.is_on(self.hass, entity):
+                return
+
+            if rgb is None or not self._same_color:
+                rgb = tuple(int(i * 255) for i in colorsys.hsv_to_rgb(random.random(), 1, 1))
+
+            logging.debug("Setting light %s to %s", entity, rgb)
+            light.turn_on(self.hass, entity_id=entity, rgb_color=rgb, white_value=0, transition=self._transition)
